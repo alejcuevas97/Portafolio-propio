d1273c5b6eed5e6d0243a70d8c1d3ec82f85aa1f
Alejandro <alejcuevas97@gmail.com>
2026-06-21 06:37:41 -0400

    bu

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-set -o errexit
+#set -o errexit
+#pip install -r requirements.txt
+#python manage.py collectstatic --noinput
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py create_admin
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py makemigrations
 python manage.py migrate
-python manage.py create_admin
